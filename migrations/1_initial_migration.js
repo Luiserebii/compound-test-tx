c33af17580dd78a2cb52e21d8416519e941ce4f2
Luiserebii <luis@serebii.io>
2021-08-13 01:53:24 -0400

    Realize I need to enter the market... though it still doesn't seem to be working?

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,13 +2,15 @@ const Migrations = artifacts.require("Migrations");
 const Fauceteer = artifacts.require("Fauceteer");
 const IERC20 = artifacts.require("IERC20");
 const CErc20Interface = artifacts.require("CErc20Interface");
+const ComptrollerInterface = artifacts.require("ComptrollerInterface");
 
 const addresses = {
   'Fauceteer': '0x67c943Cd1292DB6F41d8B3DC24494b9396cB18A6',
   'BAT': '0x0EC2F54d2eEff7AB0c5ab8E570B894c6B941D9F3',
   'DAI': '0xE503E9484C0787863d9260559D43bf1B68ea4C92',
   'cBAT': '0x49F80ddbFAD529dFe7B35158B733dc50A5f4Ef90',
-  'cDAI': '0x8bF0D4747553cFC954914643a7B65797706048fb'
+  'cDAI': '0x8bF0D4747553cFC954914643a7B65797706048fb',
+  'Unitroller': '0x641b3E3e96D2a1913340Ad4Df60E1e73D441Ab15'
 };
 
 module.exports = async (deployer, network, accounts) => {
@@ -16,24 +18,27 @@ module.exports = async (deployer, network, accounts) => {
     deployer.deploy(Migrations);
   }
 
+  //Please see: https://medium.com/compound-finance/borrowing-assets-from-compound-quick-start-guide-f5e69af4b8f4
+
   const instances = {};
   instances.Fauceteer = await Fauceteer.at(addresses.Fauceteer);
   instances.BAT = await IERC20.at(addresses.BAT);
   instances.DAI = await IERC20.at(addresses.DAI);
   instances.cBAT = await CErc20Interface.at(addresses.cBAT);
   instances.cDAI = await CErc20Interface.at(addresses.cDAI);
+  instances.Unitroller = await ComptrollerInterface.at(addresses.Unitroller);
 
   const switchboard = {
     s1: false,
     s2: true,
-    s3: false,
-    s4: false,
+    s3: false, //true,
+    s4: false //true
   }
 
   const amnts = {
     mintBAT: '4000000000000000000000',
     mintDAI: '950000000000000000000',
-    borrowBAT: '500000000000000000000'
+    borrowBAT: '50000000000000000'//'500000000000000000000'
   };
 
   if(switchboard.s1) {
@@ -60,13 +65,18 @@ module.exports = async (deployer, network, accounts) => {
   }
   if(switchboard.s2) {
     console.log('Attempting a cBAT borrow...');
+    await instances.Unitroller.enterMarkets([addresses.cDAI]);
+    console.log('B enters the market for using DAI as collateral');
     await instances.cBAT.borrow(amnts.borrowBAT, {from: accounts[1]});
     console.log(`B borrows ${amnts.borrowBAT} BAT`);
+    console.log('BAT balance of borrower', (await instances.BAT.balanceOf(accounts[1])).toString(10));
   } else {
     console.log('Skipping s2...');
   }
   if(switchboard.s3) {
     console.log('Attemping to return borrowed cBAT...');
+    await instances.BAT.approve(addresses.cBAT, amnts.borrowBAT, {from: accounts[1]});
+    console.log('B approves cBAT to transfer borrowed BAT');
     await instances.cBAT.repayBorrow(amnts.borrowBAT, {from: accounts[1]});
     console.log(`B returns ${amnts.borrowBAT} BAT`);
   } else {
@@ -81,5 +91,7 @@ module.exports = async (deployer, network, accounts) => {
   } else {
     console.log('Skipping s4...');
   }
+
+  let res = await instances.Uni;
   console.log('Finished executing test transactions! :)');
 };
