ee209db6021bacbfb99e62b8bcef398c33855050
Luiserebii <luis@serebii.io>
2021-08-16 17:50:41 -0400

    Moving address config out to file, fixing small bug that occurs when division results in non-integer BigNumber

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,20 +1,12 @@
 const Migrations = artifacts.require("Migrations");
 const Fauceteer = artifacts.require("Fauceteer");
-const CErc20Interface = artifacts.require("CErc20Interface");
+const IERC20 = artifacts.require("IERC20");
 const CErc20Immutable = artifacts.require("CErc20Immutable");
 const CErc20Delegator = artifacts.require("CErc20Delegator");
 const ComptrollerG1 = artifacts.require("ComptrollerG1");
 
 const BigNumber = require('bignumber.js');
-
-const addresses = {
-  'Fauceteer': '0xC33DcaFd15a5B5aaC1923FEAc383e37928E6f60b',
-  'BAT': '0x503E390472398d329B6501c85BBF89dF9D432445',
-  'DAI': '0x4e8ad90aF369d984bfc689a686a015aD4F47A9c1',
-  'cBAT': '0x1e81709B735d9F536B224d47f28bDd3606e6C96d',
-  'cDAI': '0x5Df444DDeec02da8d0CEc33EE35f1522c1Cb75E9',
-  'Unitroller': '0x6Ad8de99561d2B585ce1597cef3078cA1Fc20899'
-};
+const { addresses } = require('../.eth.config.js');
 
 module.exports = async (deployer, network, accounts) => {
   if(network == "development") {
@@ -74,53 +66,18 @@ module.exports = async (deployer, network, accounts) => {
   r = await instances.Unitroller.exitMarket(addresses.cBAT, {from: accounts[1]});
   logtx('B exits the cBAT market', r);
   cDAIredeem = new BigNumber((await instances.cDAI.balanceOf(accounts[1])).toString(10))
-    .multipliedBy(0.3);
-  r = await instances.cDAI.redeem(cDAIredeem.toString(10), {from: accounts[1]});
+    .multipliedBy(0.3).integerValue(BigNumber.ROUND_FLOOR);
+  r = await instances.cDAI.redeem(cDAIredeem.toString(), {from: accounts[1]});
   logtx('B redeems some cDAI for DAI', r);
   cBATredeem = new BigNumber((await instances.cBAT.balanceOf(accounts[0])).toString(10))
-    .multipliedBy(0.45);
-  r = await instances.cBAT.redeem(cBATredeem.toString(10), {from: accounts[0]});
+    .multipliedBy(0.45).integerValue(BigNumber.ROUND_FLOOR);
+  r = await instances.cBAT.redeem(cBATredeem.toString(), {from: accounts[0]});
   logtx('A redeems some cBAT for BAT', r);
 
   console.log('Finished executing test transactions! :)');
 };
 
-function recurseToString(o) {
-  for(k of Object.keys(o)) {
-    if(typeof o[k] == 'object' && o[k] != null) {
-      if(o[k].constructor.name == 'BN') {
-        o[k] = o[k].toString(10);
-      } else {
-        recurseToString(o[k]);
-      }
-    }
-  }
-  return o;
-}
-
 function logtx(msg, r) {
   console.log(msg);
   console.log(` > ${r.tx}`)
 }
-
-function logres(res) {
-  console.log(recurseToString(res));
-}
-
-/**
- * NOTE: We have to also enter the market of the asset we're borrowing.
- * Proof? Check the error message at
- *  > 0xc6e8f3b0c566958b7cca947a382dd8f31dabf8a4baf377da5098b8cecf76d2bd
- *
- * Note that the error messages align, in ErrorReporter.sol:
- * COMPTROLLER_REJECTION, BORROW_COMPTROLLER_REJECTION, MARKET_NOT_ENTERED
- *
- * Looking at the borrowAllowed() function in ComptrollerG1, we see:
- *
- *    if (!markets[cToken].accountMembership[borrower]) {
- *        return uint(Error.MARKET_NOT_ENTERED);
- *    }
- *
- * Therefore, ensure to add to market for collateral liquidity calculation
- * AND token to borrow.
- */
